7a3a3635d8845da0b2750c9178c3ee3a0bcfcd04
Christian Casal <christiancasal88@gmail.com>
2025-12-02 14:35:44 -0300

    recuperar password

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import api from '../utils/api' // <-- agregado
+import api from '../../utils/api' // <-- agregado
 
 export default function ForgotPassword(){
   const [email, setEmail] = useState('')
